109433423deec40d10805a5b56b7055c99240970
Bernardo Castelo <bernardo.castelo@gmail.com>
2021-07-26 10:03:14 +0100

    Added Home component and routing

--- WebApp/src/app/app-routing.module.ts
+++ WebApp/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const homeRoute = environment.homeRoute;
+
+
+const routes: Routes = [
+  { path: homeRoute, component: HomeComponent },
+  { path: '', redirectTo: homeRoute, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
